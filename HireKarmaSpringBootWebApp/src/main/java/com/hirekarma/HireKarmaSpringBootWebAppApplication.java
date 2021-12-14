a913621fdf0d1be3ebc84a56a4ee34bccf1c09c6
Sourav Kumar Nayak <sourav.nayak@csm.co.in>
2021-12-14 12:01:33 +0530

    Initial commit after packaging

---
+++ HireKarmaSpringBootWebApp/src/main/java/com/hirekarma/HireKarmaSpringBootWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.hirekarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireKarmaSpringBootWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HireKarmaSpringBootWebAppApplication.class, args);
+	}
+
+}

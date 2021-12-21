dbec65790fdad9280f3fab5e66a1cddfffc38155
Sourav Kumar Nayak <sourav.nayak@csm.co.in>
2021-12-21 21:16:12 +0530

    commit

--- src/main/java/com/hirekarma/HireKarmaSpringBootWebAppApplication.java
+++ src/main/java/com/hirekarma/HireKarmaSpringBootWebAppApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class HireKarmaSpringBootWebAppApplication extends SpringBootServletInitializer{
 	
@@ -17,6 +20,11 @@ public class HireKarmaSpringBootWebAppApplication extends SpringBootServletIniti
 
 	public static void main(String[] args) {
 		SpringApplication.run(HireKarmaSpringBootWebAppApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String welcome() {
+		return "Welcome to Heroku";
 	}
 
 }
